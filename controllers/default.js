1c09a09a5dcd436cb3e11bfc684426cd07072813
M ABD AZIZ ALFIAN <aalfiann@gmail.com>
2020-12-18 01:56:03 +0700

    - update

--- controllers/default.js
+++ controllers/default.js
@@ -2,8 +2,9 @@ exports.install = function () {
 	// CORS('/search/*', ['get', 'post', 'put', 'delete'], true);
 	ROUTE('/', view_index);
 	ROUTE('/perusahaan', view_perusahaan);
-	ROUTE('/search', cari_lpjk);
-	ROUTE('POST /search/viewdata/', databu, ['json']);
+	ROUTE('/about', view_about);
+	// ROUTE('/search', cari_lpjk);
+	// ROUTE('POST /search/viewdata/', databu, ['json']);
 
 	// or
 	// ROUTE('/');
@@ -17,37 +18,41 @@ function view_perusahaan() {
 	var self = this;
 	self.view('perusahaan');
 }
-function cari_lpjk() {
+function view_about() {
 	var self = this;
-	self.view('search');
+	self.view('about');
 }
-function databu() {
-	var self = this;
+// function cari_lpjk() {
+// 	var self = this;
+// 	self.view('search');
+// }
+// function databu() {
+// 	var self = this;
 
-	var data = self.body.data;
-	if (data.racord == "" || data.racord == undefined) {
-		self.json({ "status": false, "data": [] });
-	} else {
-		var unirest = require('unirest');
-		var req = unirest('POST', 'https://search.lpjk.net/search_badan_usaha/searching_bu')
-			.headers({
-				'Content-Type': 'application/x-www-form-urlencoded',
-			})
-			.send('racord=' + data.racord)
-			.send('option=' + data.option)
-			.send('status_reg=' + data.isactive)
+// 	var data = self.body.data;
+// 	if (data.racord == "" || data.racord == undefined) {
+// 		self.json({ "status": false, "data": [] });
+// 	} else {
+// 		var unirest = require('unirest');
+// 		var req = unirest('POST', 'https://search.lpjk.net/search_badan_usaha/searching_bu')
+// 			.headers({
+// 				'Content-Type': 'application/x-www-form-urlencoded',
+// 			})
+// 			.send('racord=' + data.racord)
+// 			.send('option=' + data.option)
+// 			.send('status_reg=' + data.isactive)
 
-			.end(function (res) {
-				if (res.error) {
-					self.json({ "status": false, "data": [] });
-				} else {
-					var data = JSON.parse(res.body);
-					console.log(data.record);
-					self.json({ "status": true, "data": data.record });
-				}
-			});
-	}
+// 			.end(function (res) {
+// 				if (res.error) {
+// 					self.json({ "status": false, "data": [] });
+// 				} else {
+// 					var data = JSON.parse(res.body);
+// 					console.log(data.record);
+// 					self.json({ "status": true, "data": data.record });
+// 				}
+// 			});
+// 	}
 
 
 
-}
+// }
